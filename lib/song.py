a8cfca1d5a6b31c768b99b06ef91b33d6417de2b
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2025-02-26 15:36:07 +0300

    Assignment done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
-    pass
+    
+    def __init__(self, name, artist, genre):
+     self.name = name
+     self.artist = artist
+     self.genre = genre
+     self.add_song_to_count()
+     self.add_to_genres(genre)
+     self.add_to_artists(artist)
+     self.add_to_genre_count(genre)
+     self.add_to_artist_count(artist)
+
+    count =0
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    genres = []
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+         cls.genres.append(genre)
+
+    artists = []
+    @classmethod
+    def add_to_artists(cls,artist):
+       if artist not in cls.artists:
+        cls.artists.append(artist)
+       
+
+    genre_count = {}
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+       
+
+    artist_count ={}
+    @classmethod
+    def add_to_artist_count(cls,artist):
+       if artist not in cls.artist_count:
+          cls.artist_count[artist] = 1
+       else:
+          cls.artist_count[artist] +=1
